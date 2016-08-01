d55dd0cb49d629ed10a4dbfd5c32e0f33a1d0919
Devang Bacharwar <devanggtgr8@gmail.com>
2016-08-01 21:09:56 +0530

    Validator with numverify api

---
+++ index.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+'use strict';
+
+const program = require('commander'),
+  request = require("request");
+
+
+let validateNum = (number,option) => {
+  var access_key = ;//
+  var validUrl = 'http://apilayer.net/api/validate' + '?access_key=' + access_key + '&number=' + number;
+  request(validUrl, function(error,response,body){
+     response = JSON.parse(body);
+     console.log("Valid: "+ response.valid);
+     console.log("Phone number: "+ response.international_format);
+     console.log("Country name: "+ response.country_name);
+     console.log("Location: "+ response.location);
+     console.log("Carrier: "+ response.carrier);
+     console.log("Country code: "+ response.country_code);
+  });
+};
+
+program
+  .version('1.0.0')
+  .command('[number]','Number to be validated' )
+  .description('A CLI to validate phone number')
+  //.option('','')
+  .action(validateNum);
+
+program.parse(process.argv);
